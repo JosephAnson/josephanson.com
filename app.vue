c1ed8404b776c889d47509db0e26aed797a1d8d3
Joseph Anson <josephanson@hotmail.co.uk>
2024-01-16 16:39:00 +0100

    fix: remove prose from whole page

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import '@unocss/reset/tailwind.css'
+import '@unocss/reset/tailwind-compat.css'
 import './styles/base.css'
 import './styles/prose.css'
 import './styles/animations.css'
@@ -36,28 +36,28 @@ const clipPath = computed(() => {
 </script>
 
 <template>
-  <NuxtLayout>
-    <div class="h-100dvh flex flex-col bg-black p-2 dark:bg-white md:p-3">
-      <div class="relative flex flex-auto flex-col of-hidden rounded-md bg-white py-8 dark:bg-black md:py-16">
-        <TheAnimation fragment-shader-options="1" />
-        <div
-          class="absolute inset-0 z-0 bg-secondary-900:15 transition-all duration-500"
-          :style="{
-            clipPath,
-          }"
-        />
+  <div class="h-100dvh flex flex-col bg-black p-2 dark:bg-white md:p-3">
+    <div class="relative flex flex-auto flex-col of-hidden rounded-md bg-white py-8 dark:bg-black md:py-16">
+      <TheAnimation fragment-shader-options="1" />
+      <div
+        class="absolute inset-0 z-0 bg-secondary-900:15 transition-all duration-500"
+        :style="{
+          clipPath,
+        }"
+      />
 
-        <div class="flex flex-grow-1 flex-col of-x-hidden of-y-auto">
-          <TheHeader />
+      <div class="flex flex-grow-1 flex-col of-x-hidden of-y-auto">
+        <TheHeader />
 
-          <BaseContainer class="relative z-1 text-sm md:text-base">
+        <BaseContainer class="relative z-1 text-sm md:text-base">
+          <NuxtLayout>
             <NuxtPage class="font-sans" />
-          </BaseContainer>
-          <span class="flex-auto" />
-        </div>
-
-        <TheFooter />
+          </NuxtLayout>
+        </BaseContainer>
+        <span class="flex-auto" />
       </div>
+
+      <TheFooter />
     </div>
-  </NuxtLayout>
+  </div>
 </template>
