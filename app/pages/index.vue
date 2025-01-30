4cc52fe3f5985e0e7e5336753be850eedaaf1575
Joseph Anson <josephanson@hotmail.co.uk>
2025-01-31 00:39:29 +0100

    fix: mobile optimise get in touch section
    fix: mobile optimise get in touch links

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -88,7 +88,7 @@ useSeoMeta({
       <div class="max-w-none prose">
         <p>I'm always open to discussing new projects, creative ideas, or opportunities to be part of your visions.</p>
         <p>Feel free to reach out through any of these channels:</p>
-        <div class="mt-6 flex gap-4">
+        <div class="mt-6 flex flex-col md:flex-row gap-4">
           <BaseButton
             to="mailto:me@josephanson.com"
             target="_blank"
