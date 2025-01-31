9ccfc537d7b82a1c2f0d0f1cb9b3451361745d6a
Joseph Anson <josephanson@hotmail.co.uk>
2025-01-31 16:33:28 +0100

    feat: update portfolio favicon

--- app/app.vue
+++ app/app.vue
@@ -24,9 +24,14 @@ useHead({
     { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
   ],
   link: [
-    { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
-    { rel: 'icon', type: 'image/svg+xml', href: '/nuxt.svg' },
-    { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+    { rel: 'manifest', href: '/site.webmanifest' },
+    { rel: 'mask-icon', href: '/safari-pinned-tab.svg' },
+    { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+    { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+    { rel: 'icon', type: 'image/png', href: '/favicon.png' },
+    { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/android-chrome-192x192.png' },
+    { rel: 'icon', type: 'image/png', sizes: '512x512', href: '/android-chrome-512x512.png' },
+    { rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png', href: '/apple-touch-icon.png' },
   ],
   htmlAttrs: {
     lang: 'en',
--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,10 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   unocss: true,
   vue: true,
+  jsonc: true,
+  typescript: true,
+  markdown: true,
+  yaml: true,
   rules: {
     'node/prefer-global/process': 'off',
   },
